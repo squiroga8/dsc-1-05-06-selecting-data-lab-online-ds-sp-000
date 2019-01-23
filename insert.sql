fd03e274ff07e1e3c230a5d85268258c86e39a87
Serena Quiroga <serenaquiroga@Serenas-MacBook-Pro.local>
2019-01-23 21:43:03 +0300

    Finished the lab, the only test that didn't pass is bc of the %blue% and i'm ok with that

---
+++ insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO planets (name, color, num_of_moons, mass, rings) VALUES ("Mercury", "gray", 0, 0.55, "no"), 
+("Venus", "yellow", 0, 0.82, "no"), ("Earth", "blue", 1, 1.00, "no"), 
+("Mars", "red", 2, 0.11, "no"), 
+("Jupiter", "orange", 53, 317.90, "no"),
+("Saturn", "hazel", 62, 95.19, "yes"), 
+("Uranus", "light blue", 27, 14.54, "yes"), 
+("Neptune", "dark blue", 14, 17.15, "yes"), 
+("Pluto", "brown", 5, 0.003, "no");
